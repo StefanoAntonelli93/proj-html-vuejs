f681de7f460e70a185af0f5d91dc83970af61603
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-10 16:45:58 +0200

    [UPD] New project team

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
-<script>
-
-</script>
+<script></script>
 
 <template>
-  New Project
+  <div class="container">
+    ciao
+    <h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+      magnam atque aliquid omnis. Deleniti itaque quibusdam mollitia hic sunt
+      eos aliquam atque voluptate, laudantium explicabo sint harum suscipit
+      temporibus quod!
+    </h1>
+  </div>
 </template>
 
 <style scoped></style>
