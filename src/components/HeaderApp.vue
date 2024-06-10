492fc770e8f91599458d0ed2339b979f9d625056
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-06-10 21:56:42 +0200

    [FIX] fixed centering of sections element

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -6,7 +6,7 @@ export default {
 </script>
 
 <template>
-    <div class="header-content d-flex px-5 py-4 justify-content-between">
+    <div class="header-content px-5 py-4 position-relative d-flex justify-content-between align-items-center">
         <div class="logo">
             <img src="https://demo.phlox.pro/business-2/wp-content/uploads/sites/57/2018/05/cropped-Group-39@2x.png" alt="">
         </div>
@@ -26,12 +26,18 @@ export default {
 
 <style scoped lang="scss">
 .header-content {
+    // SFUMATURA AL DI SOTTO DELL HEADER
     -webkit-box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
     -moz-box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
     box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
 }
 
 .sections {
+    // HO USATO QUESTO STILE PER CENTRARE PERFETTAMENTE LA SEZIONE CENTRALE AL CENTRO DEL PADRE A PRESCINDERE DELLA GRANDEZZA DEGLI ALTRI DUE ELEMENTI
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+
     span {
         margin-right: 5px;
     }
