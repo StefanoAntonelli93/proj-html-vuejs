cc734605d8820e599688f938ebc09fdca10f87a8
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-11 20:20:51 +0200

    [UPD] add style at h1,h2,h3

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,10 +1,22 @@
+<script>
+import TitlePage from "../components/TitlePage.vue";
+import Separator from "../components/Separator.vue";
+
+export default {
+  components: {
+    TitlePage,
+    Separator,
+  },
+};
+</script>
+
 <template>
   <!-- title -->
-  <div class="container text-center p-5">
-    <h1>Contact Page</h1>
-  </div>
+
+  <TitlePage :titlePage="'Contacts Page'"></TitlePage>
+
   <!-- map -->
-  <div class="map text-center border">
+  <div class="map text-center border mt-5">
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d172332.7219707041!2d-74.07022514757855!3d40.7520689889307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2585500e85589%3A0x18f39d1f8ee2a312!2sNew%20York%20Marriott%20Marquis!5e0!3m2!1sit!2sit!4v1718118337398!5m2!1sit!2sit"
       width="100%"
@@ -18,35 +30,35 @@
   <!-- info card -->
   <div class="info-card">
     <div class="container d-flex justify-content-between gap-5">
-      <div class="col d-flex gap-2 p-4 sfuma">
+      <div class="col d-flex gap-2 p-5 sfuma">
         <div class="align-self-center">
           <font-awesome-icon
             class="icon fa-5x"
             icon="fa-solid fa-location-dot"
           />
         </div>
-        <div class="content p-5">
+        <div class="content p-4">
           <div class="fw-semibold py-3 fs-3">ADDRESS:</div>
           <div>123 Ave, Lorem City, site Country, The World</div>
         </div>
       </div>
-      <div class="col d-flex gap-2 p-4 sfuma">
+      <div class="col d-flex gap-2 p-5 sfuma">
         <div class="align-self-center">
           <font-awesome-icon
             class="icon fa-5x"
             icon="fa-solid fa-phone-volume"
           />
         </div>
-        <div class="content p-5">
+        <div class="content p-4">
           <div class="fw-semibold py-3 fs-3">PHONE:</div>
           <div>(001) 123456789 - 234567891 info@phloxbusiness.com</div>
         </div>
       </div>
-      <div class="col d-flex gap-2 p-4 sfuma">
+      <div class="col d-flex gap-2 p-5 sfuma">
         <div class="align-self-center">
           <font-awesome-icon class="icon fa-5x" icon="fa-solid fa-stopwatch" />
         </div>
-        <div class="content p-5">
+        <div class="content p-4">
           <div class="fw-semibold py-3 fs-3">WORK HOURS:</div>
           <div>
             Monday - Friday <span class="px-2">09.00 - 23.00</span> Sunday
@@ -57,12 +69,26 @@
     </div>
   </div>
   <!-- send message -->
-  <div class="send-message">
-    <div class="container">
-      <div class="row justify-content-between border">
-        <div class="col-4">with-us</div>
-        <div class="col-6">send message</div>
+  <div class="send-messag ps-3">
+    <div class="row justify-content-between">
+      <div class="col-4">
+        <div class="content py-5">
+          <div class="h1">
+            Please get in <span class="highlighted-text">touch</span> with
+            <span class="highlighted-text">us</span>
+          </div>
+          <Separator class="py-5" />
+          <p>
+            When, while the lovely valley teems with vapour around me, and the
+            meridian sun strikes the upper surface of the impenetrable foliage
+            of my trees, and but a few stray gleams steal into the inner
+            sanctuary.When, while the lovely valley teems with vapour around me,
+            and the meridian sun strikes the upper surface of the impenetrable
+            foliage of my trees.
+          </p>
+        </div>
       </div>
+      <div class="col-6">send message</div>
     </div>
   </div>
 </template>
@@ -74,7 +100,8 @@
   -moz-box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
   box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
 }
-.icon {
+.icon,
+.highlighted-text {
   color: $secondary-color;
 }
 </style>
