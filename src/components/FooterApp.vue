2b48e2564de1f50524fac93d08bf59cee96091e5
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-11 03:28:38 +0200

    [UPD] end footer-dark

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -4,7 +4,59 @@ export default {
 };
 </script>
 <template>
-  <div class="footer-background">
+  <div class="footer-background-white">
+    <div class="container d-flex flex-wrap justify-content-center py-5">
+      <div class="col">
+        <div class="content">
+          <img
+            draggable="false"
+            src="https://demo.phlox.pro/business-2/wp-content/uploads/sites/57/2018/05/cropped-Group-39@2x.png"
+            alt=""
+          />
+          <div class="text-logo py-3">
+            While the lovely valley teems with vapour around me, and the
+            meridian sun strikes the upper surface of the impenetrable foliage
+            of my trees.While the lovely valley teems with vapour around me.
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="content">
+          <h2 class="title">quick links</h2>
+          <hr />
+          <ul class="list-unstyled">
+            <li><a class="text-decoration-none" href="">Careers</a></li>
+            <li><a class="text-decoration-none" href=""> News</a></li>
+            <li><a class="text-decoration-none" href="">Terms of use</a></li>
+            <li>
+              <a class="text-decoration-none" href="">Privacy Projects</a>
+            </li>
+            <li><a class="text-decoration-none" href="">About</a></li>
+            <li><a class="text-decoration-none" href="">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col">
+        <div class="content">
+          <div class="title">titolo</div>
+          <hr />
+          <ul>
+            <li><a href="">link</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col">
+        <div class="content">
+          <div class="title">titolo</div>
+          <hr />
+          <ul>
+            <li><a href="">link</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-background-dark">
     <div class="container d-flex flex-wrap justify-content-between py-5">
       <p>© 2024 PHLOX BUSINESS THEME.</p>
       <div class="social-links d-flex align-items-center gap-4">
@@ -18,8 +70,11 @@ export default {
 
 <style scoped lang="scss">
 @use "../assets/scss/partials/_variables.scss" as *;
-.footer-background {
+.footer-background-dark {
   background: linear-gradient(90deg, $primary-color 40%, rgba(0, 0, 0, 1) 100%);
   color: $secondary-text-color;
+}
+li a {
+  color: black;
 }
 </style>
