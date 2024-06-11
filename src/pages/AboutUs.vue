161ffaf1da66c65a302000b69e1b9aae2ca8ff4c
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-06-11 03:27:46 +0200

    [UPD] styled about us page

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -13,15 +13,25 @@ export default {
 <template>
     <TitlePage :titlePage="'About Page'"></TitlePage>
     <section>
-        <div class="we-are">
-            <div class="text-area">
-                <h2>JSHFLIwuhjlifjHBWLKEJFClwe</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nobis mollitia, eius tenetur repellat dolore
-                    distinctio illum. Enim natus cum hic ducimus! Modi facere deleniti assumenda dolore non sequi aperiam?
+        <div class="we-are d-flex">
+            <div class="text-area d-flex gap-5 flex-column">
+                <h2>We are a creative web
+                    <div class="secondary-color">design agency</div>
+                </h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+                    blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
+                    ocean.Separated they live in Bookmarksgrove.
                 </p>
+                <div>
+                    <div class="button">
+                        READ MORE
+                    </div>
+                </div>
+
             </div>
             <div class="img-area">
-
+                <img src="https://demo.phlox.pro/business-2/wp-content/uploads/sites/57/2018/08/woman-photographer-drinking-coffee-and-working-PJNBP6U-1024x754.jpg"
+                    alt="">
             </div>
         </div>
     </section>
@@ -32,8 +42,7 @@ export default {
 
 .we-are {
     margin: 130px 0;
-    padding: 130px 0;
-    height: 500px;
+    padding: 130px 10px;
     background: linear-gradient(to right,
             $section-color 0%,
             $section-color 72%,
@@ -41,11 +50,20 @@ export default {
             white 100%);
 
     .text-area {
-        width: 45%;
+        padding: 50px 50px 0 0;
+        width: 43%;
+
+        h2 {
+            font-size: 3.5rem;
+        }
     }
 
     .img-area {
-        width: 55%;
+        width: 57%;
+
+        img {
+            width: 100%;
+        }
     }
 }
 </style>
