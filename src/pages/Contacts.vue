5e3723a4fd31a20c57b6c25ec0a46851e8f7656c
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-11 17:13:54 +0200

    [UPD] install package google maps

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,1 +1,34 @@
-<template><h1>Contacts</h1></template>
+<template>
+  <!-- title -->
+  <div class="container text-center p-5">
+    <h1>Contact Page</h1>
+  </div>
+  <!-- map -->
+  <div class="map text-center border">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d172332.7219707041!2d-74.07022514757855!3d40.7520689889307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2585500e85589%3A0x18f39d1f8ee2a312!2sNew%20York%20Marriott%20Marquis!5e0!3m2!1sit!2sit!4v1718118337398!5m2!1sit!2sit"
+      width="100%"
+      height="600"
+      style="border: 0"
+      allowfullscreen="true"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  </div>
+  <!-- info card -->
+  <div class="info-card">
+    <div class="container d-flex justify-content-between text-center border">
+      <div class="col">1</div>
+      <div class="col">2</div>
+      <div class="col">3</div>
+    </div>
+  </div>
+  <!-- send message -->
+  <div class="send-message">
+    <div class="container">
+      <div class="row justify-content-between border">
+        <div class="col-4">with-us</div>
+        <div class="col-6">send message</div>
+      </div>
+    </div>
+  </div>
+</template>
