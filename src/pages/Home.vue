aae65b598e5661a05e58f35ab0b84f9005b43810
annaritadituri <annaritadituri@icloud.com>
2024-06-11 12:48:36 +0200

    [UPD] Added basic HTML to home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,1 +1,417 @@
-<template><h1>Home</h1></template>
+<script>
+    import EmailComponent from '../components/EmailComponent.vue';
+    import PartnersComponent from '../components/PartnersComponent.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            EmailComponent,
+            PartnersComponent,
+        }
+    }
+
+</script>
+
+<template>
+
+    <!--jumbo-->
+    <div class="container-fluid">
+        <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-8">
+                
+            </div>
+            <div class="col-4">
+                
+            </div>
+        </div>
+
+    </div>
+    </div>
+
+    <!--we are-->
+    <!--DA RIEMPIRE DINAMICAMENTE-->
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-4">
+                <h2>
+                    We Are Here To
+                    Make YourWebsite
+                    Look More Elegant And Stylish!
+                </h2>
+                <div class="button">
+                    VIEW ALL
+                </div>
+            </div>
+            <div class="col-8">
+
+                <div class="row">
+
+                    <div class="col">
+
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Far far away, behind the word mountains, far from the countries Vokalia Separated...
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Far far away, behind the word mountains, far from the countries Vokalia Separated...
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="row">
+
+                    <div class="col">
+
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Far far away, behind the word mountains, far from the countries Vokalia Separated...
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col">
+
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Far far away, behind the word mountains, far from the countries Vokalia Separated...
+                                </p>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                </div>
+                
+            </div>
+
+        </div>
+
+    </div>
+
+    <!--learn more-->
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-6">
+                
+            </div>
+            <div class="col-6">
+                <h2>
+                    learn more about our missions
+                </h2>
+                <p>
+                    Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.Separated they live in Bookmarksgrove.
+                </p>
+                <div class="button">
+                    READ MORE
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!--past projects-->
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-6">
+                <h2>
+                    Our recentweb designs &
+                    some examples of
+                    past projects
+                </h2>
+            </div>
+            <div class="col-6">
+                <p>
+                    Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.Separated they live in Bookmarksgrove.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <!--examples-->
+    <!--DA RIEMPIRE DINAMICAMENTE-->
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                    <img src="..." class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">The Basket of flowers</h5>
+                        <p class="card-text">BRANDING</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                    <img src="..." class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">The Basket of flowers</h5>
+                        <p class="card-text">BRANDING</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                    <img src="..." class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">The Basket of flowers</h5>
+                        <p class="card-text">BRANDING</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                    <img src="..." class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">The Basket of flowers</h5>
+                        <p class="card-text">BRANDING</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                    <img src="..." class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">The Basket of flowers</h5>
+                        <p class="card-text">BRANDING</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                    <img src="..." class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">The Basket of flowers</h5>
+                        <p class="card-text">BRANDING</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                    <img src="..." class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">The Basket of flowers</h5>
+                        <p class="card-text">BRANDING</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card text-bg-dark">
+                    <img src="..." class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">The Basket of flowers</h5>
+                        <p class="card-text">BRANDING</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!--we love-->
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-6 text-center">
+                <div class="row">
+                    <div class="col">
+                        <h2>280</h2>
+                        <h5>PROJECTS</h5>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>+3,500</h4>
+                        <h5>USERS' COMMENTS</h5>
+                    </div>
+                    <div class="col">
+                        <h4>100%</h4>
+                        <h5>HAPPY CLIENTS</h5>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-6">
+                <h2>
+                    WeLove What
+                    We Do
+                </h2>
+                <p>
+                    Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.Separated they live in Bookmarksgrove. Far far away, behind the word mountains. large language ocean. far from the countries Vokalia and Consonantia
+                </p>
+                <ul>
+                    <li>far far away, behind the word mountains</li>
+                    <li>large language ocean</li>
+                    <li>far from the countries Vokalia and Consonantia</li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+    <!--plans-->
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-4">
+                <h2>
+                    With Live Chat
+                    24/7 Support
+                </h2>
+                <p>
+                    Far far away, behind the word mountains, far from the countriesVokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics.
+                </p>
+            </div>
+            <div class="col-8">
+                <div class="row">
+                    <div class="col text-center">
+                        <h5>
+                            DESIGNING
+                        </h5>
+                        <p>
+                            All Designs
+                        </p>
+                        <h2>$ 10 / Design</h2>
+                        <ul>
+                            <li>Creative Design Enabled</li>
+                            <li>Vibrant Color Usage</li>
+                            <li>Eye Catching Design</li>
+                            <li>Extreme Typography</li>
+                            <li>Exceptional Design</li>
+                        </ul>
+                    </div>
+                    <div class="col text-center">
+                        <h5>
+                            DEVELOPMENT
+                        </h5>
+                        <p>
+                            WordPress Projects
+                        </p>
+                        <h2>$ 15 / Design</h2>
+                        <ul>
+                            <li>Creative Design Enabled</li>
+                            <li>Vibrant Color Usage</li>
+                            <li>Eye Catching Design</li>
+                            <li>Extreme Typography</li>
+                            <li>Exceptional Design</li>
+                        </ul>
+                    </div>
+                    <div class="col text-center">
+                        <h5>
+                            SEO
+                        </h5>
+                        <p>
+                            Web Products
+                        </p>
+                        <h2>$ 10 / Design</h2>
+                        <ul>
+                            <li>Creative Design Enabled</li>
+                            <li>Vibrant Color Usage</li>
+                            <li>Eye Catching Design</li>
+                            <li>Extreme Typography</li>
+                            <li>Exceptional Design</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!--reviews-->
+    <div class="container-fluid text-center">
+        <p>
+            When it comes to barbequing, there are two main schools of thought for the techniques that you can use. Freshly scrambled eggs with applewood smoked bacon - are amazing!
+        </p>
+        <img src="" alt="">
+        <h5>
+            LISSA DURBIN
+        </h5>
+        <p>
+            Client
+        </p>
+    </div>
+
+    <!--blog-->
+    <div class="container-fluid text-center">
+        <h2>Latest News & Our Blog</h2>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">May 24, 2018 - by Alex</p>
+                        <h4>Marketing Ideas</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">May 24, 2018 - by Alex</p>
+                        <h4>Marketing Ideas</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">May 24, 2018 - by Alex</p>
+                        <h4>Marketing Ideas</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="button">VIEW ALL POSTS</div>
+    </div>
+
+    <!--contact-->
+    <EmailComponent />
+
+    <!--partners-->
+    <PartnersComponent />
+
+</template>
+
+<style scoped lang="scss">
+
+.col-3 {
+    height: 300px;
+
+    .card {
+        height: 100%;
+    }
+}
+
+
+</style>
