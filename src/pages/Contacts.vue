a9f669689b141c962f4d81cf50c748a9234d49ec
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-11 22:59:56 +0200

    [UPD] add form to Contacts page

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -29,8 +29,8 @@ export default {
   </div>
   <!-- info card -->
   <div class="info-card">
-    <div class="container d-flex justify-content-between gap-5">
-      <div class="col d-flex gap-2 p-5 sfuma">
+    <div class="container d-flex justify-content-between gap-4">
+      <div class="col d-flex gap-2 p-5 shadows">
         <div class="align-self-center">
           <font-awesome-icon
             class="icon fa-5x"
@@ -42,7 +42,7 @@ export default {
           <div>123 Ave, Lorem City, site Country, The World</div>
         </div>
       </div>
-      <div class="col d-flex gap-2 p-5 sfuma">
+      <div class="col d-flex gap-2 p-5 shadows">
         <div class="align-self-center">
           <font-awesome-icon
             class="icon fa-5x"
@@ -54,15 +54,15 @@ export default {
           <div>(001) 123456789 - 234567891 info@phloxbusiness.com</div>
         </div>
       </div>
-      <div class="col d-flex gap-2 p-5 sfuma">
+      <div class="col d-flex gap-2 p-5 shadows">
         <div class="align-self-center">
           <font-awesome-icon class="icon fa-5x" icon="fa-solid fa-stopwatch" />
         </div>
-        <div class="content p-4">
+        <div class="content p-3">
           <div class="fw-semibold py-3 fs-3">WORK HOURS:</div>
           <div>
-            Monday - Friday <span class="px-2">09.00 - 23.00</span> Sunday
-            <span class="px-2">09.00 - 16.00</span>
+            <p>Monday-Friday 09.00-23.00</p>
+            <p>Sunday 09.00-16.00</p>
           </div>
         </div>
       </div>
@@ -70,7 +70,7 @@ export default {
   </div>
   <!-- send message -->
   <div class="p-5">
-    <div class="row justify-content-around">
+    <div class="row justify-content-around align-items-center">
       <div class="col-4">
         <div class="content py-5">
           <div class="h2">
@@ -90,16 +90,38 @@ export default {
       </div>
       <div class="col-6">
         <div class="mb-3">
-          <div class="h3 fw-bold">Send message</div>
-          <label for="exampleFormControlInput1" class="form-label"
-            >Email address</label
-          >
-          <input
-            type="email"
-            class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-          />
+          <div class="h3 fw-bold py-3">Send message</div>
+          <div class="row justify-content-center">
+            <div class="col">
+              <label for="Fullname" class="form-label">Fullname*</label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+              />
+            </div>
+            <div class="col">
+              <label for="exampleFormControlInput1" class="form-label"
+                >Email address</label
+              >
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+            <div class="col">
+              <label for="exampleFormControlInput1" class="form-label"
+                >Please Insert Phone number</label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="exampleFormControlInput1"
+              />
+            </div>
+          </div>
         </div>
         <div class="mb-3">
           <label for="exampleFormControlTextarea1" class="form-label"
@@ -111,6 +133,7 @@ export default {
             rows="3"
           ></textarea>
         </div>
+        <button class="button p-3 fw-bold">SEND NOW</button>
       </div>
     </div>
   </div>
@@ -118,7 +141,7 @@ export default {
 
 <style scoped lang="scss">
 @use "../assets/scss/partials/_variables.scss" as *;
-.sfuma {
+.shadows {
   -webkit-box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
   -moz-box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
   box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
