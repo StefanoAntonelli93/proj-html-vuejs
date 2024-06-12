b643ca6f9646e4ec6eed4d72eca6a4fefff03a27
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-06-12 19:38:43 +0200

    [UPD] finish OurTeamCard

--- src/components/OurTeamCard.vue
+++ src/components/OurTeamCard.vue
@@ -21,11 +21,17 @@ export default {
 <template>
     <div class="card-image" :style="setBgImg">
         <div class="card-content">
-            <p>{{ card.name }}</p>
+            <div class="h3">{{ card.name }}</div>
             <p>{{ card.role }}</p>
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Conson, there live the blind
                 texts. Separated they live in Bookmarksgrove right at the coast of the Semantics...
             </p>
+            <hr>
+            <div class="social-links d-flex align-items-center gap-4">
+                <font-awesome-icon icon="fa-brands fa-facebook-f" />
+                <font-awesome-icon icon="fa-brands fa-twitter" />
+                <font-awesome-icon icon="fa-brands fa-pinterest-p" />
+            </div>
         </div>
 
     </div>
@@ -40,13 +46,24 @@ export default {
     background-size: cover;
     background-position: center;
 
+    &:hover .card-content {
+        display: block;
+        background: rgb(153, 225, 94);
+        background: linear-gradient(225deg, rgba(153, 225, 94, 0.95) 0%, rgba(0, 217, 166, 0.95) 100%);
+    }
+
     .card-content {
-        width: 100%;
+        display: none;
+        padding: 70px 50px;
+        border-radius: 10px;
+        color: white;
         position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
+        top: -10px;
+        left: -10px;
+        right: -10px;
+        bottom: -10px;
+
+
     }
 }
 </style>
