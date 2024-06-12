f161eeefc6eee9801701f63be4260e0f01eff537
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-12 16:27:46 +0200

    [UPD] create components 'ContactsCard'

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,11 +1,13 @@
 <script>
 import TitlePage from "../components/TitlePage.vue";
 import Separator from "../components/Separator.vue";
+import ContactsCard from "../components/ContactsCard.vue";
 
 export default {
   components: {
     TitlePage,
     Separator,
+    ContactsCard,
   },
 };
 </script>
@@ -28,49 +30,7 @@ export default {
       ></iframe>
     </div>
     <!-- info card -->
-    <div class="info-card">
-      <div class="container d-flex justify-content-between gap-4">
-        <div class="col d-flex gap-2 p-5 shadows">
-          <div class="align-self-center">
-            <font-awesome-icon
-              class="icon fa-5x"
-              icon="fa-solid fa-location-dot"
-            />
-          </div>
-          <div class="content p-4">
-            <div class="fw-semibold py-3 fs-3">ADDRESS:</div>
-            <div>123 Ave, Lorem City, site Country, The World</div>
-          </div>
-        </div>
-        <div class="col d-flex gap-2 p-5 shadows">
-          <div class="align-self-center">
-            <font-awesome-icon
-              class="icon fa-5x"
-              icon="fa-solid fa-phone-volume"
-            />
-          </div>
-          <div class="content p-4">
-            <div class="fw-semibold py-3 fs-3">PHONE:</div>
-            <div>(001) 123456789 - 234567891 info@phloxbusiness.com</div>
-          </div>
-        </div>
-        <div class="col d-flex gap-2 p-5 shadows">
-          <div class="align-self-center">
-            <font-awesome-icon
-              class="icon fa-5x"
-              icon="fa-solid fa-stopwatch"
-            />
-          </div>
-          <div class="content p-3">
-            <div class="fw-semibold py-3 fs-3">WORK HOURS:</div>
-            <div>
-              <p>Monday-Friday 09.00-23.00</p>
-              <p>Sunday 09.00-16.00</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ContactsCard />
   </div>
 
   <!-- send message -->
@@ -154,24 +114,6 @@ export default {
 
   .map {
     position: relative;
-  }
-}
-
-.info-card {
-  position: absolute;
-  top: 450px;
-  left: 20px;
-  width: calc(100% - 40px);
-  z-index: 10;
-  .shadows {
-    background-color: white;
-    -webkit-box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
-    -moz-box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
-    box-shadow: 0px 10px 37px 2px rgba(0, 0, 0, 0.3);
-  }
-  .icon,
-  .highlighted-text {
-    color: $secondary-color;
   }
 }
 
