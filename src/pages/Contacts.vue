f7bb2dd9fad16372fde006164fc533f69770781d
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-12 11:25:09 +0200

    [UPD] add style font ,h1,h2.h3 at typography

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -16,53 +16,58 @@ export default {
   <TitlePage :titlePage="'Contacts Page'"></TitlePage>
 
   <!-- map -->
-  <div class="map text-center border mt-5">
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d172332.7219707041!2d-74.07022514757855!3d40.7520689889307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2585500e85589%3A0x18f39d1f8ee2a312!2sNew%20York%20Marriott%20Marquis!5e0!3m2!1sit!2sit!4v1718118337398!5m2!1sit!2sit"
-      width="100%"
-      height="600"
-      style="border: 0"
-      allowfullscreen="true"
-      referrerpolicy="no-referrer-when-downgrade"
-      zoom="5"
-    ></iframe>
-  </div>
-  <!-- info card -->
-  <div class="info-card">
-    <div class="container d-flex justify-content-between gap-4">
-      <div class="col d-flex gap-2 p-5 shadows">
-        <div class="align-self-center">
-          <font-awesome-icon
-            class="icon fa-5x"
-            icon="fa-solid fa-location-dot"
-          />
+  <div class="section-map position-relative">
+    <div class="map text-center border mt-5">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d172332.7219707041!2d-74.07022514757855!3d40.7520689889307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2585500e85589%3A0x18f39d1f8ee2a312!2sNew%20York%20Marriott%20Marquis!5e0!3m2!1sit!2sit!4v1718118337398!5m2!1sit!2sit"
+        width="100%"
+        height="600"
+        style="border: 0"
+        allowfullscreen="true"
+        referrerpolicy="no-referrer-when-downgrade"
+        zoom="5"
+      ></iframe>
+    </div>
+    <!-- info card -->
+    <div class="info-card position-absolute">
+      <div class="container d-flex justify-content-between gap-4">
+        <div class="col d-flex gap-2 p-5 shadows">
+          <div class="align-self-center">
+            <font-awesome-icon
+              class="icon fa-5x"
+              icon="fa-solid fa-location-dot"
+            />
+          </div>
+          <div class="content p-4">
+            <div class="fw-semibold py-3 fs-3">ADDRESS:</div>
+            <div>123 Ave, Lorem City, site Country, The World</div>
+          </div>
         </div>
-        <div class="content p-4">
-          <div class="fw-semibold py-3 fs-3">ADDRESS:</div>
-          <div>123 Ave, Lorem City, site Country, The World</div>
+        <div class="col d-flex gap-2 p-5 shadows">
+          <div class="align-self-center">
+            <font-awesome-icon
+              class="icon fa-5x"
+              icon="fa-solid fa-phone-volume"
+            />
+          </div>
+          <div class="content p-4">
+            <div class="fw-semibold py-3 fs-3">PHONE:</div>
+            <div>(001) 123456789 - 234567891 info@phloxbusiness.com</div>
+          </div>
         </div>
-      </div>
-      <div class="col d-flex gap-2 p-5 shadows">
-        <div class="align-self-center">
-          <font-awesome-icon
-            class="icon fa-5x"
-            icon="fa-solid fa-phone-volume"
-          />
-        </div>
-        <div class="content p-4">
-          <div class="fw-semibold py-3 fs-3">PHONE:</div>
-          <div>(001) 123456789 - 234567891 info@phloxbusiness.com</div>
-        </div>
-      </div>
-      <div class="col d-flex gap-2 p-5 shadows">
-        <div class="align-self-center">
-          <font-awesome-icon class="icon fa-5x" icon="fa-solid fa-stopwatch" />
-        </div>
-        <div class="content p-3">
-          <div class="fw-semibold py-3 fs-3">WORK HOURS:</div>
-          <div>
-            <p>Monday-Friday 09.00-23.00</p>
-            <p>Sunday 09.00-16.00</p>
+        <div class="col d-flex gap-2 p-5 shadows">
+          <div class="align-self-center">
+            <font-awesome-icon
+              class="icon fa-5x"
+              icon="fa-solid fa-stopwatch"
+            />
+          </div>
+          <div class="content p-3">
+            <div class="fw-semibold py-3 fs-3">WORK HOURS:</div>
+            <div>
+              <p>Monday-Friday 09.00-23.00</p>
+              <p>Sunday 09.00-16.00</p>
+            </div>
           </div>
         </div>
       </div>
@@ -155,5 +160,12 @@ export default {
   border-bottom: 2px solid #000;
   outline: none;
   box-shadow: none;
+}
+.info-card {
+  z-index: 99;
+  bottom: -80px;
+  background-color: white;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
