1f0fd0c807d3ce2962fa337931f6ca18d6502b3c
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-06-13 12:50:43 +0200

    [UPD] add HomeSupport component

---
+++ src/components/HomeSupport.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+    name: "HomeSupport",
+};
+</script>
+<template>
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-4">
+                <h2>
+                    With Live Chat
+                    24/7 Support
+                </h2>
+                <p>
+                    Far far away, behind the word mountains, far from the countriesVokalia and Consonantia, there live the
+                    blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics.
+                </p>
+            </div>
+            <div class="col-8">
+                <div class="row">
+                    <div class="col text-center">
+                        <h5>
+                            DESIGNING
+                        </h5>
+                        <p>
+                            All Designs
+                        </p>
+                        <h2>$ 10 / Design</h2>
+                        <ul>
+                            <li>Creative Design Enabled</li>
+                            <li>Vibrant Color Usage</li>
+                            <li>Eye Catching Design</li>
+                            <li>Extreme Typography</li>
+                            <li>Exceptional Design</li>
+                        </ul>
+                    </div>
+                    <div class="col text-center">
+                        <h5>
+                            DEVELOPMENT
+                        </h5>
+                        <p>
+                            WordPress Projects
+                        </p>
+                        <h2>$ 15 / Design</h2>
+                        <ul>
+                            <li>Creative Design Enabled</li>
+                            <li>Vibrant Color Usage</li>
+                            <li>Eye Catching Design</li>
+                            <li>Extreme Typography</li>
+                            <li>Exceptional Design</li>
+                        </ul>
+                    </div>
+                    <div class="col text-center">
+                        <h5>
+                            SEO
+                        </h5>
+                        <p>
+                            Web Products
+                        </p>
+                        <h2>$ 10 / Design</h2>
+                        <ul>
+                            <li>Creative Design Enabled</li>
+                            <li>Vibrant Color Usage</li>
+                            <li>Eye Catching Design</li>
+                            <li>Extreme Typography</li>
+                            <li>Exceptional Design</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<style scoped lang="scss"></style>
