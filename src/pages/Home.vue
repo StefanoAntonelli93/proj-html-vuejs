3a45ab97ea555a62c19ceb878fc06c55b7382ed5
annaritadituri <annaritadituri@icloud.com>
2024-06-13 13:20:40 +0200

    [UPD] Added Home Support section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,13 @@
 <script>
 
 import PartnerSection from '../components/PartnerSection.vue';
+import HomeSupport from '../components/HomeSupport.vue';
 
 export default {
     name: 'Home',
     components: {
         PartnerSection,
+        HomeSupport,
     }
 }
 
@@ -292,75 +294,10 @@ export default {
 
     </div>
 
-    <!--plans-->
-    <div class="container-fluid">
-
-        <div class="row">
-            <div class="col-4">
-                <h2>
-                    With Live Chat
-                    24/7 Support
-                </h2>
-                <p>
-                    Far far away, behind the word mountains, far from the countriesVokalia and Consonantia, there live the
-                    blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics.
-                </p>
-            </div>
-            <div class="col-8">
-                <div class="row">
-                    <div class="col text-center">
-                        <h5>
-                            DESIGNING
-                        </h5>
-                        <p>
-                            All Designs
-                        </p>
-                        <h2>$ 10 / Design</h2>
-                        <ul>
-                            <li>Creative Design Enabled</li>
-                            <li>Vibrant Color Usage</li>
-                            <li>Eye Catching Design</li>
-                            <li>Extreme Typography</li>
-                            <li>Exceptional Design</li>
-                        </ul>
-                    </div>
-                    <div class="col text-center">
-                        <h5>
-                            DEVELOPMENT
-                        </h5>
-                        <p>
-                            WordPress Projects
-                        </p>
-                        <h2>$ 15 / Design</h2>
-                        <ul>
-                            <li>Creative Design Enabled</li>
-                            <li>Vibrant Color Usage</li>
-                            <li>Eye Catching Design</li>
-                            <li>Extreme Typography</li>
-                            <li>Exceptional Design</li>
-                        </ul>
-                    </div>
-                    <div class="col text-center">
-                        <h5>
-                            SEO
-                        </h5>
-                        <p>
-                            Web Products
-                        </p>
-                        <h2>$ 10 / Design</h2>
-                        <ul>
-                            <li>Creative Design Enabled</li>
-                            <li>Vibrant Color Usage</li>
-                            <li>Eye Catching Design</li>
-                            <li>Extreme Typography</li>
-                            <li>Exceptional Design</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
+    <!--support-->
+    <section>
+        <HomeSupport />
+    </section>
 
     <!--reviews-->
     <div class="container-fluid text-center reviews">
