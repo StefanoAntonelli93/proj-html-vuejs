23511a57e27e906a95e4e4fb49e37cbf54203e4e
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-13 17:39:05 +0200

    [UPD] HomeStatistics

--- src/components/HomeStatistics.vue
+++ src/components/HomeStatistics.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
   <!--we love-->
-  <div class="container-fluid">
+  <div class="container-fluid main-padding">
     <div class="row">
       <div class="col-6 text-center">
         <div class="row">
@@ -32,26 +32,33 @@ export default {
           </div>
         </div>
       </div>
-      <div class="col-6">
-        <h2>WeLove What We Do</h2>
-        <p>
-          Far far away, behind the word mountains, far from the countries
-          Vokalia and Consonantia, there live the blind texts. Separated they
-          live in Bookmarksgrove right at the coast of the Semantics, a large
-          language ocean.Separated they live in Bookmarksgrove. Far far away,
-          behind the word mountains. large language ocean. far from the
-          countries Vokalia and Consonantia
-        </p>
-        <ul>
-          <li>far far away, behind the word mountains</li>
-          <li>large language ocean</li>
-          <li>far from the countries Vokalia and Consonantia</li>
-        </ul>
+      <div class="col-6 p-5">
+        <div class="content p-5 d-flex flex-column gap-4">
+          <div class="h2 title">
+            We <span class="green-text">Love</span> What We Do
+            <span class="green-text"></span>
+          </div>
+          <Separator />
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts. Separated they
+            live in Bookmarksgrove right at the coast of the Semantics, a large
+            language ocean.Separated they live in Bookmarksgrove. Far far away,
+            behind the word mountains. large language ocean. far from the
+            countries Vokalia and Consonantia
+          </p>
+          <ul>
+            <li>far far away, behind the word mountains</li>
+            <li>large language ocean</li>
+            <li>far from the countries Vokalia and Consonantia</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped lang="scss">
+@use "../assets/scss/partials/_variables.scss" as *;
 .background-image {
   background-image: url("https://demo.phlox.pro/business-2/wp-content/uploads/sites/57/2018/08/person-with-long-curly-hair-PZ99QM2@2x.jpg");
   width: 100%;
@@ -67,5 +74,8 @@ export default {
     rgba(153, 225, 94, 0.8) 0%,
     rgba(0, 217, 166, 0.8) 100%
   );
+}
+.green-text {
+  color: $secondary-color;
 }
 </style>
